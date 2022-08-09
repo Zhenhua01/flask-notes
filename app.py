000254f1b45abce1a6ed6fe388bd921a57263182
Zhenhua <zhenhua1109@yahoo.com>
2022-08-09 12:33:57 -0700

    create forms model

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 """Flask app for flask notes"""
 
-from flask import Flask, request, jsonify, render_template
+from crypt import methods
+from flask import Flask, request, jsonify, render_template, redirect
 from models import db, connect_db, Notes
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///notes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
+
+@app.get('/')
+def homepage():
+    """Redirects to registration page"""
+
+    return redirect('/register')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register_new_user():
+    """Shows registration form, updates database, and redirects to secrete page"""
+
---
+++ forms.py
@@ -0,0 +1,29 @@
+"""Forms Model"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
+
+
+class RegisterForm(FlaskForm):
+    """Form for registering a user."""
+
+    username = StringField("Username",
+                            validators=[InputRequired(),
+                            Length(max=20)])
+
+    password = PasswordField("Password",
+                            validators=[InputRequired(),
+                            Length(max=100)])
+
+    email = StringField("Email",
+                        validators=[Email(),
+                        Length(max=50)])
+
+    first_name = StringField("First Name",
+                            validators=[InputRequired(),
+                            Length(max=30)])
+
+    last_name = StringField("Last Name",
+                            validators=[InputRequired(),
+                            Length(max=30)])
